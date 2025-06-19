3507eb2cd65442c19cd612d5eb4c9de0ab7941df
Ritik kumar <41362257+Ritik-06101997@users.noreply.github.com>
2025-06-19 23:53:50 +0530

    Update Coding Challenge.py

--- Coding Challenge.py
+++ Coding Challenge.py
@@ -24,4 +24,28 @@ class Solution:
                 continue
         return output
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+2. Replace all 0's with 5
+Difficulty: BasicAccuracy: 75.55%Submissions: 93K+Points: 1Average Time: 15m
+You are given an integer n. You need to convert all zeroes of n to 5.
+
+Examples:
+
+Input: n = 1004
+Output: 1554
+Explanation: There are two zeroes in 1004 on replacing all zeroes with 5, the new number will be 1554.
+Input: n = 121
+Output: 121
+Explanation: Since there are no zeroes in 121, the number remains as 121.
+# Function should return an integer value
+class Solution:
+    def convertFive(self, n):
+        # Code here
+        strn = str(n)
+        res = ''
+        for char in strn:
+            if char == '0':
+                res = res + '5'
+            else:
+                res += char
+        return res
 
